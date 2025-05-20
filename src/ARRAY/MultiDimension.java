0f7915fdb61d01dd9a77e3dc3d7aad7a92613d0a
sarthakpawar0912 <pawarsr06@gmail.com>
2025-05-20 22:25:18 +0530

    Fixed bugs and updated code

--- src/ARRAY/MultiDimension.java
+++ src/ARRAY/MultiDimension.java
@@ -27,6 +27,7 @@ public class MultiDimension {
         System.out.println(arr.length);
 
         for (int row=0;row<arr.length;row++){
+
             //for each col in every row
             for (int col=0;col<arr[row].length;col++){
                 System.out.println("Enter the value for arr["+row+"]["+col+"]:");
