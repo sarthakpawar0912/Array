eabc4a0016ca77e0a46f8e39aee5960db19ae525
sarthakpawar0912 <pawarsr06@gmail.com>
2025-05-20 22:28:17 +0530

    Fixed bugs and updated code

--- src/ARRAY/BasicInput.java
+++ src/ARRAY/BasicInput.java
@@ -7,6 +7,7 @@ public class BasicInput {
     public static void main(String[] args) {
 
         Scanner sc=new Scanner(System.in);
+
         int arr[]=new int[5];
         arr[0]=10;
         arr[1]=20;
