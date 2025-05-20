cda6368071fb296db82ff03dd22c52aefb7d6416
sarthakpawar0912 <pawarsr06@gmail.com>
2025-05-20 22:28:32 +0530

    Fixed bugs and updated code

--- src/ARRAY/BasicInput.java
+++ src/ARRAY/BasicInput.java
@@ -22,7 +22,9 @@ public class BasicInput {
 
         }
 
+
         System.out.println(Arrays.toString(arr));
+
 
         for (int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
