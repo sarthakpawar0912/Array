8d597f3ee3b0c4e9c5a8607a00f64f72e708d652
sarthakpawar0912 <pawarsr06@gmail.com>
2025-05-20 22:25:45 +0530

    Fixed bugs and updated code

--- src/ARRAY/MultiDimension.java
+++ src/ARRAY/MultiDimension.java
@@ -42,6 +42,7 @@ public class MultiDimension {
         for (int row=0;row<arr.length;row++){
 
             for (int col=0;col<arr[row].length;col++){
+
                 System.out.print(arr[row][col]+" ");
             }
             System.out.println();
