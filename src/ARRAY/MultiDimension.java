eba521e9aa89d15c9100b1dc3d50167369e60cb5
sarthakpawar0912 <pawarsr06@gmail.com>
2025-05-21 17:38:27 +0530

    Fixed bugs and updated code

--- src/ARRAY/MultiDimension.java
+++ src/ARRAY/MultiDimension.java
@@ -13,10 +13,13 @@ public class MultiDimension {
 //        };
 
 //        int[][] arr2D={
+
 //                {1,2,3},//0th index
 //                {4,5},  //1st index
 //                {6,7,8,9} //2nd index----------->arr2D[2]={6,7,8,9}
-//        };
+//
+//
+//      };
 
         Scanner sc=new Scanner(System.in);
 
