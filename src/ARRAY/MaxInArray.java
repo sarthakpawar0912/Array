1212f257b8d574dc1bd48da239ea7335749f229e
sarthakpawar0912 <pawarsr06@gmail.com>
2025-05-21 17:33:00 +0530

    Fixed bugs and updated code

--- src/ARRAY/MaxInArray.java
+++ src/ARRAY/MaxInArray.java
@@ -13,7 +13,9 @@ public class MaxInArray {
     static int max(int[] arr) {
 
     int maxVal = arr[0];
+
     for (int i = 0; i < arr.length; i++) {
+
         if (arr[i] > maxVal) {
             maxVal = arr[i];
         }
