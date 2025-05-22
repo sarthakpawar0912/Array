9367c8bd2d1ca429467cb7434e8bb6d4999dbf69
sarthakpawar0912 <pawarsr06@gmail.com>
2025-05-22 21:57:58 +0530

    Fixed bugs and updated code

--- src/ARRAY/EXAMPLES/BuyANDSell_Stocks.java
+++ src/ARRAY/EXAMPLES/BuyANDSell_Stocks.java
@@ -41,6 +41,7 @@ public class BuyANDSell_Stocks {
 
         // Test case 1: prices = [7,1,5,3,6,4]
         int[] prices1 = {7, 1, 5, 3, 6, 4};
+
         System.out.println("Test case 1: Max profit = " + solution.maxProfit(prices1)); // Expected: 5
 
         // Test case 2: prices = [7,6,4,3,1]
