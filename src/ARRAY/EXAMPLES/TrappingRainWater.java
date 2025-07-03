ef37899c466b87eefd2520ae0b94bc8a750fbdc4
sarthakpawar0912 <pawarsr06@gmail.com>
2025-07-03 23:53:15 +0530

    Fixed bugs and updated code

--- src/ARRAY/EXAMPLES/TrappingRainWater.java
+++ src/ARRAY/EXAMPLES/TrappingRainWater.java
@@ -50,6 +50,7 @@ public class TrappingRainWater {
 
         // Test case 1: height = [0,1,0,2,1,0,1,3,2,1,2,1]
         int[] height1 = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+
         System.out.println("Test case 1: Water trapped = " + solution.trap(height1)); // Expected: 6
 
         // Test case 2: height = [4,2,0,3,2,5]
